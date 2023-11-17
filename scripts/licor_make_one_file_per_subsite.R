a0ffc119c8f956ef8cb83a346a48df1f3ff40fdc
camille.minaudo <camille.minaudo@ub.edu>
2023-11-17 09:58:04 +0000

    adapting to new licor data architecture

--- scripts/licor_make_one_file_per_subsite.R
+++ scripts/licor_make_one_file_per_subsite.R
@@ -27,6 +27,7 @@ library(egg)
 library(GoFluxYourself)
 require(dplyr)
 require(purrr)
+require(pbapply)
 
 
 source(paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/click.peak.R"))
@@ -41,7 +42,7 @@ source(paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/import2RData.
 
 # ---- Directories ----
 dropbox_root <- "C:/Users/Camille Minaudo/Dropbox/RESTORE4Cs - Fieldwork/Data"
-datapath <- paste0(dropbox_root,"/GHG/RAW data")
+datapath <- paste0(dropbox_root,"/GHG/RAW data/RAW Data Licor-7810")
 fieldsheetpath <- paste0(dropbox_root,"/GHG/Fieldsheets")
 
 # ---- List GHG chamber fieldsheets in Dropbox ----
@@ -63,6 +64,8 @@ for (f in myfieldsheets_list){
   names(fieldsheet_temp) <- my_headers
   fieldsheet_temp <- fieldsheet_temp[!is.na(fieldsheet_temp$plot_id),]
   fieldsheet_temp$date <- as.Date( fieldsheet_temp$date, tryFormats = c("%d.%m.%y", "%d/%m/%y"))
+  fieldsheet_temp$subsiteID <- gsub(pattern = "-Fieldsheet-GHG.xlsx", replacement = "", x = basename(f))
+
 
   if(isF){
     isF <- F
@@ -75,27 +78,26 @@ for (f in myfieldsheets_list){
 
 fieldsheet <- fieldsheet[!is.na(fieldsheet$longitude),]
 
-
+fieldsheet_licor <- fieldsheet[fieldsheet$gas_analyzer=="LI-COR",]
 
 
 # ---- Import and store measurements to RData ----
-data_folders <- list.dirs(datapath, full.names = T, recursive = F)[-2]
+data_folders <- list.dirs(datapath, full.names = T, recursive = F)
+r <- grep(pattern = "RData",x=data_folders)
+data_folders <- data_folders[-r]
+
 message("Here is the list of data folders in here:")
 print(data_folders)
 
+map_analysers <- data.frame(instrument = c("LI-7810"),
+                            date.format = c("ymd"))
 
-i_licor <- grep(pattern = "Licor", x = data_folders)
-i_losgatos <- grep(pattern = "Los Gatos", x = data_folders)
-i_picarro <- grep(pattern = "Picarro", x = data_folders)
-map_analysers <- data.frame(instrument = c("LI-7810","LGR","G2508"),
-                            i = c(i_licor, i_losgatos, i_picarro),
-                            date.format = c("ymd","mdy","ymd"))
-k=0
-for (data_folder in data_folders){
-  k=k+1
+mainDirRData = paste(datapath,"/RData",sep="")
+
+for (data_folder in data_folders[2]){
   setwd(data_folder)
-  import2RData(path = data_folder, instrument = map_analysers$instrument[k],
-               date.format = map_analysers$date.format[k], timezone = 'UTC')
+  import2RData(path = data_folder, instrument = map_analysers$instrument,
+               date.format = map_analysers$date.format, timezone = 'UTC')
 
   # load all these R.Data into a single dataframe
   file_list <- list.files(path = paste(data_folder,"/RData",sep=""), full.names = T)
@@ -112,18 +114,38 @@ for (data_folder in data_folders){
   }
 
   # get read of possible duplicated data
-  mydata_imp_clean <- mydata_imp %>%
-    group_by(DATE,TIME) %>% # the complete group of interest
-    mutate(duplicate = n()) %>% # count number in each group
-    filter(duplicate == 1) %>% # select only unique records
-    select(-duplicate) # remove group count column
-
+  is_duplicate <- duplicated(mydata_imp$POSIX.time)
+  mydata_imp_clean <- mydata_imp[!is_duplicate,]
 
   # save this dataframe as a new RData file
-  setwd(data_folder)
-  save(mydata_imp_clean, file = "data_all_clean.RData")
+  setwd(mainDirRData)
+  save(mydata_imp_clean, file = paste0(basename(data_folder),"_data_clean.RData"))
 
+
+  # create separate folders for each subsite where data actually exists
+  for (subsite in unique(fieldsheet_licor$subsiteID)){
+    corresponding_date <- unique(fieldsheet_licor$date[fieldsheet_licor$subsiteID == subsite])
+
+    # check if we already have this data somewhere in the clean file
+    n_lines_d <- length(mydata_imp_clean$DATE[mydata_imp_clean$DATE == corresponding_date])
+
+    if(n_lines_d > 0){
+      message(paste0("... there is some data to store for subsite ",subsite))
+      mydata <- mydata_imp_clean[mydata_imp_clean$DATE == corresponding_date,]
+
+      # creating a folder where to put this data
+      dir.create(file.path(mainDirRData, subsite))
+      setwd(file.path(mainDirRData, subsite))
+
+
+      # save this dataframe as a new RData file
+      setwd(file.path(mainDirRData, subsite))
+      save(mydata, file = paste0("data_",subsite,".RData"))
+    }
+  }
 }
+
+
 
 
 
