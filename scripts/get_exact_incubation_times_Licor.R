c8504e159aa2204b3c8526b5f9b755b801d85dbe
camille.minaudo <camille.minaudo@ub.edu>
2023-11-24 16:18:15 +0100

    adapted the mapping of all LICOR incubations

--- scripts/get_exact_incubation_times_Licor.R
+++ scripts/get_exact_incubation_times_Licor.R
@@ -7,7 +7,7 @@
 
 # --- Description of this script
 # This script extracts the exact datetime for each chamber measurements performed
-# with the Picarro Gas Scouter. It loads the corresponding fieldworksheet and
+# with the LiCor. It loads the corresponding fieldworksheet and
 # produce a new fieldwork sheet with corrected time stamps
 
 rm(list = ls()) # clear workspace
@@ -30,68 +30,83 @@ require(purrr)
 
 
 source(paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/get_unix_times.R"))
-
-
-read_map_incubations <- function(path2folder){
-
-  my_maps_filenames <- list.files(data_folder, pattern = ".csv", all.files = T, full.names = T, recursive = F)
-  isF <- T
-  for(my_maps_filename in my_maps_filenames){
-
-    map_incubations_temp <- read.csv(file = my_maps_filename,
-                                     header = T)
-    map_incubations_temp <- map_incubations_temp[map_incubations_temp$Species ==  "CH4",]
-
-    if(isF){
-      isF <- F
-      map_incubations <- map_incubations_temp
-    } else {
-      map_incubations <- rbind(map_incubations, map_incubations_temp)
-    }
-  }
-
-  map_incubations <- data.frame(subsite = basename(path2folder),
-                                plot = map_incubations$Comment,
-                                time_code = map_incubations$Time.Code,
-                                start = map_incubations$start_fit,
-                                stop = map_incubations$end_fit)
-
-  return(map_incubations)
-}
-
+source(paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/read_Licor.R"))
 
 
 
 # ---- Directories ----
 dropbox_root <- "C:/Users/Camille Minaudo/Dropbox/RESTORE4Cs - Fieldwork/Data"
-datapath <- paste0(dropbox_root,"/GHG/RAW data/RAW Data Picarro")
+datapathRAW <- paste0(dropbox_root,"/GHG/RAW data/RAW Data Licor-7810")
+datapath <- paste0(dropbox_root,"/GHG/RAW data/RAW Data Licor-7810/RData")
 fieldsheetpath <- paste0(dropbox_root,"/GHG/Fieldsheets")
 path2processed <- paste0(dropbox_root,"/GHG/Processed data")
 
-# ---- List folders with Picarro data in Dropbox ----
+# ---- List folders with Licor data in Dropbox ----
 data_folders <- list.dirs(datapath, full.names = T, recursive = F)
 subsites <- basename(data_folders)
+print(subsites)
 
-k=0
+# go through the RAW data
+fs <- list.files(path = datapathRAW, pattern = c(".txt", ".data"), full.names = T, recursive = T)
+r <- grep(pattern = ".RData",x=fs)
+fs <- fs[-r]
+
+
+isF = T
+for (f in fs){
+  message(paste0("extracting info from ",basename(f)))
+  data <- read_Licor(file = f)
+  labels <- unique(data$label)
+  labels<- labels[-which(labels=="")]
+
+  # get start and stop times for each label
+  for (label in labels){
+    start <- min(data$unixtime[data$label == label])
+    stop <- max(data$unixtime[data$label == label])
+
+    map_incubations_temp <- data.frame(date = first(data$date[data$label == label]),
+                                       label = label,
+                                       start = start,
+                                       stop = stop,
+                                       time_start = strftime(start, format="%H:%M:%S", tz = 'utc'),
+                                       time_stop = strftime(stop, format="%H:%M:%S", tz = 'utc'),
+                                       folder = basename(dirname(f)),
+                                       file = basename(f))
+    if(isF){
+      isF = F
+      map_incubations <- map_incubations_temp
+    } else {
+      map_incubations <- rbind(map_incubations,map_incubations_temp)
+    }
+  }
+}
+
+
+# uniqueID <- paste0(map_incubations$date, map_incubations$label)
+map_incubations <- map_incubations[!duplicated(map_incubations$start),]
+map_incubations <- map_incubations[order(map_incubations$start),]
+
+# Save incubtion map to Dropbox
+write.csv(x = map_incubations, file = paste0(datapathRAW,"/map_incubations.csv"), row.names = F)
+
+
+# k=0
 for (subsite in subsites){
   message(paste0("processing data for ",subsite))
-  k=k+1
-  data_folder <- data_folders[k]
-
-  # read all the csv files in data_folder, and group into a single one
-  map_incubations <- read_map_incubations(path2folder = data_folder)
+  # k=k+1
+  # data_folder <- data_folders[k]
 
   # load corresponding fieldsheet
   pilotsite <- substr(subsite, 1, 5)
   f_name <- paste0(fieldsheetpath,"/",pilotsite,"/",subsite,"-Fieldsheet-GHG.xlsx")
+  message(f_name)
 
   fieldsheet <- readxl::read_xlsx(f_name, col_names = T)# Read it first to get the headers
   my_headers <- names(fieldsheet)
   fieldsheet <- readxl::read_xlsx(f_name, col_names = F, range = "A3:V50")# re-read and affect correctly the headers
   names(fieldsheet) <- my_headers
   fieldsheet <- fieldsheet[!is.na(fieldsheet$plot_id),]
-  fieldsheet$date <- as.Date( fieldsheet$date, tryFormats = c("%d.%m.%y", "%d/%m/%y"))
-
+  fieldsheet$date <- as.Date( fieldsheet$date, tryFormats = c("%d.%m.%Y", "%d/%m/%Y"))
 
   fieldsheet$unix_start <- get_unix_times(mydate = fieldsheet$date, mytime = fieldsheet$start_time)
   fieldsheet$unix_stop <- get_unix_times(mydate = fieldsheet$date, mytime = fieldsheet$end_time)
@@ -104,30 +119,35 @@ for (subsite in subsites){
 
   for (i in seq_along(fieldsheet$plot_id)){
     ind <- which.min(abs(fieldsheet$unix_start[i] - map_incubations$start))
+    # corresponding_row[i] <- ind
     if(abs(fieldsheet$unix_start[i] - map_incubations$start[ind])<3*60){corresponding_row[i] <- ind}
   }
+  isNA <- !is.na(corresponding_row)
   corresponding_row <- corresponding_row[!is.na(corresponding_row)]
-  map_incubations <- map_incubations[corresponding_row, ]
+  my_map_incubations <- map_incubations[corresponding_row, ]
 
-  if(dim(map_incubations)[1] != dim(fieldsheet)[1]){
-    fieldsheet <- fieldsheet[seq_along(corresponding_row),]
+  if(dim(my_map_incubations)[1] >0){
+    message("....found corresponding labels for this one")
+    fieldsheet <- fieldsheet[isNA,]
+
+    fieldsheet$start_time <- strftime(fieldsheet$start_time, format="%H:%M:%S", tz = 'utc')
+    fieldsheet$end_time <- strftime(fieldsheet$end_time, format="%H:%M:%S", tz = 'utc')
+
+    fieldsheet$unix_start_corrected <- my_map_incubations$start
+    fieldsheet$unix_stop_corrected <- my_map_incubations$stop
+
+    fieldsheet$timestamp_start <- as.POSIXct(fieldsheet$unix_start_corrected, tz = "UTC", origin = "1970-01-01")
+    fieldsheet$timestamp_stop <- as.POSIXct(fieldsheet$unix_stop_corrected, tz = "UTC", origin = "1970-01-01")
+
+    fieldsheet$start_time_corrected <- strftime(fieldsheet$timestamp_start, format="%H:%M:%S", tz = 'utc')
+    fieldsheet$end_time_corrected <- strftime(fieldsheet$timestamp_stop, format="%H:%M:%S", tz = 'utc')
+
+    setwd(paste0(path2processed,"/corrected_fieldsheets"))
+    f_name <- paste0(subsite,"-Fieldsheet-GHG_corrected.csv")
+    write.csv(file = f_name, x = fieldsheet, row.names = F)
+
   }
 
-  fieldsheet$start_time <- strftime(fieldsheet$start_time, format="%H:%M:%S", tz = 'utc')
-  fieldsheet$end_time <- strftime(fieldsheet$end_time, format="%H:%M:%S", tz = 'utc')
-
-  fieldsheet$unix_start_corrected <- map_incubations$start
-  fieldsheet$unix_stop_corrected <- map_incubations$stop
-
-  fieldsheet$timestamp_start <- as.POSIXct(fieldsheet$unix_start_corrected, tz = "UTC", origin = "1970-01-01")
-  fieldsheet$timestamp_stop <- as.POSIXct(fieldsheet$unix_stop_corrected, tz = "UTC", origin = "1970-01-01")
-
-  fieldsheet$start_time_corrected <- strftime(fieldsheet$timestamp_start, format="%H:%M:%S", tz = 'utc')
-  fieldsheet$end_time_corrected <- strftime(fieldsheet$timestamp_stop, format="%H:%M:%S", tz = 'utc')
-
-  setwd(paste0(path2processed,"/corrected_fieldsheets"))
-  f_name <- paste0(subsite,"-Fieldsheet-GHG_corrected.csv")
-  write.csv(file = f_name, x = fieldsheet, row.names = F)
 
 }
 
