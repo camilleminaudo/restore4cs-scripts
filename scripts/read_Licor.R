343f0f05bc76e674f1c65da631b9f6e2dcc6904d
camille.minaudo <camille.minaudo@ub.edu>
2023-11-24 16:18:29 +0100

    small edit

--- scripts/read_Licor.R
+++ scripts/read_Licor.R
@@ -1,18 +1,17 @@
 
-
+source(paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/get_unix_times.R"))
 
 
 read_Licor <- function(file){
-  message(paste0("reading ",file_to_read," with script for ",analyser," gas analyser"))
-  filename <- paste(datapath,directory_analyser,file_to_read, sep = "/")
-  data_raw <- read_lines(filename)
-  prefix <- substr(data_raw, start = 1,stop = 5) # isolte first 5 characters for each line
+  message(paste0("reading ",file))
+  data_raw <- read_lines(file)
+  prefix <- substr(data_raw, start = 1,stop = 5) # isolate first 5 characters for each line
 
   # find line corresponding to headers
   headers <- unlist(strsplit(data_raw[which(prefix == "DATAH")], "\t"))
   units <- unlist(strsplit(data_raw[which(prefix == "DATAU")], "\t"))
 
-  data <- read.delim(filename, sep = "\t", header = F, skip = which(prefix == "DATAU"), na.strings = "nan")
+  data <- read.delim(file, sep = "\t", header = F, skip = which(prefix == "DATAU"), na.strings = "nan")
   names(data) <- headers
 
   my_data <- data.frame(date = data$DATE,
