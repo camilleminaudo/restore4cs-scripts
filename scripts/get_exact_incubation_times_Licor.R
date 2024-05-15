921eb6465829fbc61e3a644a72f417de335cc64f
camille.minaudo <camille.minaudo@ub.edu>
2024-05-15 12:08:52 +0200

    rerun to get the latest incubation map

--- scripts/get_exact_incubation_times_Licor.R
+++ scripts/get_exact_incubation_times_Licor.R
@@ -29,9 +29,9 @@ require(purrr)
 # ---- functions ----
 
 
-source(paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/get_unix_times.R"))
-source(paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/read_Licor.R"))
-
+repo_root <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
+files.sources = list.files(path = paste0(repo_root,"/functions"), full.names = T)
+for (f in files.sources){source(f)}
 
 
 # ---- Directories ----
@@ -56,15 +56,18 @@ isF = T
 for (f in fs){
   message(paste0("extracting info from ",basename(f)))
   data <- read_Licor(file = f)
-  labels <- unique(data$label)
-  labels<- labels[-which(labels=="")]
+  labels <- unique((data$label))
+  labels <- labels[!is.na(labels)]
+  # labels<- labels[-which(labels=="")]
 
   # get start and stop times for each label
   for (label in labels){
-    start <- min(data$unixtime[data$label == label])
-    stop <- max(data$unixtime[data$label == label])
+    ind_corresp <- which(data$label == label)
+    
+    start <- min(data$unixtime[ind_corresp])
+    stop <- max(data$unixtime[ind_corresp])
 
-    map_incubations_temp <- data.frame(date = first(data$date[data$label == label]),
+    map_incubations_temp <- data.frame(date = first(data$date[ind_corresp]),
                                        label = label,
                                        start = start,
                                        stop = stop,
@@ -85,71 +88,133 @@ for (f in fs){
 # uniqueID <- paste0(map_incubations$date, map_incubations$label)
 map_incubations <- map_incubations[!duplicated(map_incubations$start),]
 map_incubations <- map_incubations[order(map_incubations$start),]
+map_incubations <- map_incubations[order(map_incubations$start),]
 
 # Save incubtion map to Dropbox
 write.csv(x = map_incubations, file = paste0(datapathRAW,"/map_incubations.csv"), row.names = F)
 
 
-# k=0
-for (subsite in subsites){
-  message(paste0("processing data for ",subsite))
-  # k=k+1
-  # data_folder <- data_folders[k]
-
-  # load corresponding fieldsheet
-  pilotsite <- substr(subsite, 1, 5)
-  f_name <- paste0(fieldsheetpath,"/",pilotsite,"/",subsite,"-Fieldsheet-GHG.xlsx")
-  message(f_name)
-
-  fieldsheet <- readxl::read_xlsx(f_name, col_names = T)# Read it first to get the headers
-  my_headers <- names(fieldsheet)
-  fieldsheet <- readxl::read_xlsx(f_name, col_names = F, range = "A3:V50")# re-read and affect correctly the headers
-  names(fieldsheet) <- my_headers
-  fieldsheet <- fieldsheet[!is.na(fieldsheet$plot_id),]
-  fieldsheet$date <- as.Date( fieldsheet$date, tryFormats = c("%d.%m.%Y", "%d/%m/%Y"))
-
-  fieldsheet$unix_start <- get_unix_times(mydate = fieldsheet$date, mytime = fieldsheet$start_time)
-  fieldsheet$unix_stop <- get_unix_times(mydate = fieldsheet$date, mytime = fieldsheet$end_time)
 
 
-  # check the closest incubation in map_incubations for each row in fieldsheet.
-  # if more than 3 minutes apart, we consider the row in map_incubations out of sampling
 
-  corresponding_row <- NA*fieldsheet$plot_id
 
-  for (i in seq_along(fieldsheet$plot_id)){
-    ind <- which.min(abs(fieldsheet$unix_start[i] - map_incubations$start))
-    # corresponding_row[i] <- ind
-    if(abs(fieldsheet$unix_start[i] - map_incubations$start[ind])<3*60){corresponding_row[i] <- ind}
+# ---- Load fieldsheets ----
+# list filenames
+myfieldsheets_list <- list.files(fieldsheetpath, pattern = "Fieldsheet-GHG.xlsx", all.files = T, full.names = T, recursive = T)
+# i <- grep(pattern = sampling, x = myfieldsheets_list) # selecting the files corresponding to the selected sampling campaign
+# myfieldsheets_list <- myfieldsheets_list[i]
+# Read all fieldsheets and put them in a single dataframe
+fieldsheet <- read_GHG_fieldsheets(myfieldsheets_list)
+
+fieldsheet$uniqID <- tolower(paste(fieldsheet$subsite,fieldsheet$plot_id,substr(fieldsheet$transparent_dark, 1, 1), sep = "-"))
+
+
+
+# check the closest incubation in map_incubations for each row in fieldsheet.
+# if more than 4 minutes apart, we consider the row in map_incubations out of sampling
+fieldsheet_Licor <- fieldsheet[fieldsheet$gas_analyzer=="LI-COR",]
+corresponding_row <- NA*fieldsheet_Licor$plot_id
+
+
+for (i in seq_along(fieldsheet_Licor$plot_id)){
+  ind <- which.min(abs(fieldsheet_Licor$unix_start[i] - map_incubations$start))
+  if(length(ind)>0){
+    if(abs(fieldsheet_Licor$unix_start[i] - map_incubations$start[ind])<5*60){
+      corresponding_row[i] <- ind
+    }
   }
-  isNA <- !is.na(corresponding_row)
-  corresponding_row <- corresponding_row[!is.na(corresponding_row)]
-  my_map_incubations <- map_incubations[corresponding_row, ]
-
-  if(dim(my_map_incubations)[1] >0){
-    message("....found corresponding labels for this one")
-    fieldsheet <- fieldsheet[isNA,]
-
-    fieldsheet$start_time <- strftime(fieldsheet$start_time, format="%H:%M:%S", tz = 'utc')
-    fieldsheet$end_time <- strftime(fieldsheet$end_time, format="%H:%M:%S", tz = 'utc')
-
-    fieldsheet$unix_start_corrected <- my_map_incubations$start
-    fieldsheet$unix_stop_corrected <- my_map_incubations$stop
-
-    fieldsheet$timestamp_start <- as.POSIXct(fieldsheet$unix_start_corrected, tz = "UTC", origin = "1970-01-01")
-    fieldsheet$timestamp_stop <- as.POSIXct(fieldsheet$unix_stop_corrected, tz = "UTC", origin = "1970-01-01")
-
-    fieldsheet$start_time_corrected <- strftime(fieldsheet$timestamp_start, format="%H:%M:%S", tz = 'utc')
-    fieldsheet$end_time_corrected <- strftime(fieldsheet$timestamp_stop, format="%H:%M:%S", tz = 'utc')
-
-    setwd(paste0(path2processed,"/corrected_fieldsheets"))
-    f_name <- paste0(subsite,"-Fieldsheet-GHG_corrected.csv")
-    write.csv(file = f_name, x = fieldsheet, row.names = F)
-
-  }
-
-
 }
+
+# finding corresponding row in case of NA in corresponding_row
+if(sum(is.na(corresponding_row))>0){
+  ind_NAs <- which(is.na(corresponding_row))
+  # it is the most probable that the missing info is in-between two rows with all the data we need
+  interp <- approx(x = seq_along(fieldsheet_Licor$plot_id), y = corresponding_row, xout = ind_NAs)$y
+  is_integer <- (interp - floor(interp)) == 0
+  corresponding_row[ind_NAs][is_integer] <- interp[is_integer]
+}
+if(sum(is.na(corresponding_row))>0){
+  ind_NAs <- which(is.na(corresponding_row))
+  message("Could not find a corresponding incubation map for the following incubations:")
+  fieldsheet_Licor$uniqID[ind_NAs]
+  # removing rows from fieldsheet_Licor with missing correspondance
+  fieldsheet_Licor <- fieldsheet_Licor[-ind_NAs,]
+  corresponding_row <- corresponding_row[-ind_NAs]
+}
+
+# replacing unix_startand unix_stop with new values
+fieldsheet_Licor$unix_start <- map_incubations$start[corresponding_row]
+fieldsheet_Licor$unix_stop <- map_incubations$stop[corresponding_row]
+
+
+
+
+
+
+
+
+
+# 
+# # k=0
+# for (subsite in subsites){
+#   message(paste0("processing data for ",subsite))
+#   # k=k+1
+#   # data_folder <- data_folders[k]
+# 
+#   # load corresponding fieldsheet
+#   pilotsite <- substr(subsite, 1, 5)
+#   f_name <- paste0(fieldsheetpath,"/",pilotsite,"/",subsite,"-Fieldsheet-GHG.xlsx")
+#   message(f_name)
+# 
+#   fieldsheet <- readxl::read_xlsx(f_name, col_names = T)# Read it first to get the headers
+#   my_headers <- names(fieldsheet)
+#   fieldsheet <- readxl::read_xlsx(f_name, col_names = F, range = "A3:V50")# re-read and affect correctly the headers
+#   names(fieldsheet) <- my_headers
+#   fieldsheet <- fieldsheet[!is.na(fieldsheet$plot_id),]
+#   fieldsheet$date <- as.Date( fieldsheet$date, tryFormats = c("%d.%m.%Y", "%d/%m/%Y"))
+# 
+#   fieldsheet$unix_start <- get_unix_times(mydate = fieldsheet$date, mytime = fieldsheet$start_time)
+#   fieldsheet$unix_stop <- get_unix_times(mydate = fieldsheet$date, mytime = fieldsheet$end_time)
+# 
+# 
+#   # check the closest incubation in map_incubations for each row in fieldsheet.
+#   # if more than 3 minutes apart, we consider the row in map_incubations out of sampling
+# 
+#   corresponding_row <- NA*fieldsheet$plot_id
+# 
+#   for (i in seq_along(fieldsheet$plot_id)){
+#     ind <- which.min(abs(fieldsheet$unix_start[i] - map_incubations$start))
+#     # corresponding_row[i] <- ind
+#     if(abs(fieldsheet$unix_start[i] - map_incubations$start[ind])<3*60){corresponding_row[i] <- ind}
+#   }
+#   isNA <- !is.na(corresponding_row)
+#   corresponding_row <- corresponding_row[!is.na(corresponding_row)]
+#   my_map_incubations <- map_incubations[corresponding_row, ]
+# 
+#   if(dim(my_map_incubations)[1] >0){
+#     message("....found corresponding labels for this one")
+#     fieldsheet <- fieldsheet[isNA,]
+# 
+#     fieldsheet$start_time <- strftime(fieldsheet$start_time, format="%H:%M:%S", tz = 'utc')
+#     fieldsheet$end_time <- strftime(fieldsheet$end_time, format="%H:%M:%S", tz = 'utc')
+# 
+#     fieldsheet$unix_start_corrected <- my_map_incubations$start
+#     fieldsheet$unix_stop_corrected <- my_map_incubations$stop
+# 
+#     fieldsheet$timestamp_start <- as.POSIXct(fieldsheet$unix_start_corrected, tz = "UTC", origin = "1970-01-01")
+#     fieldsheet$timestamp_stop <- as.POSIXct(fieldsheet$unix_stop_corrected, tz = "UTC", origin = "1970-01-01")
+# 
+#     fieldsheet$start_time_corrected <- strftime(fieldsheet$timestamp_start, format="%H:%M:%S", tz = 'utc')
+#     fieldsheet$end_time_corrected <- strftime(fieldsheet$timestamp_stop, format="%H:%M:%S", tz = 'utc')
+# 
+#     setwd(paste0(path2processed,"/corrected_fieldsheets"))
+#     f_name <- paste0(subsite,"-Fieldsheet-GHG_corrected.csv")
+#     write.csv(file = f_name, x = fieldsheet, row.names = F)
+# 
+#   }
+# 
+# 
+# }
 
 
 
