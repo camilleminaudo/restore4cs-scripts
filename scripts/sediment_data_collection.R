a768397008d2afb61101c5cbb5d2d72d1011012f
bemisteli <88994877+bemisteli@users.noreply.github.com>
2024-09-30 14:02:49 +0200

    Updated to work with final datasets

--- scripts/sediment_data_collection.R
+++ scripts/sediment_data_collection.R
@@ -32,7 +32,6 @@ source(paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/get_unix_time
 #dropbox_root <- "C:/Users/Camille Minaudo/Dropbox/RESTORE4Cs - Fieldwork/Data/" #Camille Minaudo
 #dropbox_root <- "D:/Dropbox/RESTORE4Cs - Fieldwork/Data/Sediment/" #Benjamin Misteli PC
 dropbox_root <- "C:/Users/misteli/Dropbox/RESTORE4Cs - Fieldwork/Data/Sediment" #Benjamin Misteli Laptop
-sampling <- "S1"
 setwd(dropbox_root)
 
 # list files in Dropbox
@@ -57,49 +56,18 @@ row.names(complete_list) <- NULL
 complete_list_sediments <- complete_list %>%
   filter(type == "sediment sample")
 
-datasheet_wcl_names <- readxl::read_xlsx("Lab data/Datasheet-Sediment.xlsx",col_names = T)
-datasheet_wcl <- readxl::read_xlsx("Lab data/Datasheet-Sediment.xlsx",skip = 2,col_names = F)
-names(datasheet_wcl) <- tolower(names(datasheet_wcl_names))
+write.xlsx(complete_list_sediments, file = "Lab data/fieldsheet_all_summary.xlsx")
 
-datasheet_wcl <- datasheet_wcl[, -c(2:12)]
-datasheet_wcl <- datasheet_wcl[!is.na(datasheet_wcl[, 1]), ]
+fieldsheet_all_summary <- readxl::read_xlsx("Lab data/fieldsheet_all_summary.xlsx",col_names = T)
+data_afdm <- readxl::read_xlsx("Lab data/LabData_dry_afdm.xlsx",col_names = T)
+data_afdm_fieldsheet <- merge(fieldsheet_all_summary, data_afdm, by = "sample_id", all.x = TRUE)
 
-merged_data <- merge(complete_list_sediments, datasheet_wcl, by = "sample_id", all = TRUE)
-merged_data <- merged_data %>%
-  arrange(desc(package_arrived))
+data_cn <- readxl::read_xlsx("Lab data/LabData_cn.xlsx",col_names = T)
+data_afdm_cn_fieldsheet <- merge(data_afdm_fieldsheet, data_cn, by = "bowl_id", all.x = TRUE)
 
-write.xlsx(merged_data, file = "Lab data/Datasheet-Sediment.xlsx")
+data_afdm_cn_fieldsheet <- data_afdm_cn_fieldsheet %>%
+  select(-c(1,3:8, 14:23, 25:27)) %>%
+  rename(comments_fieldsheet = comments)%>%
+  separate(sample_id, into = c("Season", "Site", "Subsite", "Replicate"), sep = "-", remove = FALSE)
 
-
-# Define the columns for which you want to create boxplots
-columns_of_interest <- c("oc_content", "dry_weight_percentage", "ash_free_dry_mass_percentage")  # Add columns as needed
-
-# Extract unique values of 'pilot_site'
-unique_pilot_site <- unique(merged_data$pilot_site)
-
-for (column in columns_of_interest) {
-  plot_list <- list()
-  
-  for (CP in unique_pilot_site) {
-    # Filter data for the current pilot_site and column
-    country_data <- filter(merged_data, pilot_site == CP)
-    
-    # Create a plot for the current column and pilot_site
-    boxplot <- ggplot(data = country_data, aes(x = subsite, y = .data[[column]])) + 
-      geom_boxplot() + 
-      theme(text = element_text(size = 16)) + 
-      scale_color_viridis_d() +
-      ggtitle(paste("Case Pilot:", CP))
-    
-    plot_list[[as.character(CP)]] <- boxplot
-  }
-  
-  # Arrange the combined plots for the current column
-  combined_plots <- ggpubr::ggarrange(plotlist = plot_list, ncol = 2, nrow = 3, common.legend = TRUE, legend = "right")
-  
-  # Save the combined plots for the current column
-  png(filename = paste0(column, "_Boxplots.png"), width = 12, height = 10, units = "in", res = 300)
-  print(combined_plots)  # Use print() to display and save the plot
-  dev.off()
-}
-
+write.xlsx(data_afdm_cn_fieldsheet, file = "Lab data/RESTORE4Cs_Sediment_Data_complete.xlsx")
